294a278e64ec7afd92f8aae7181566138abecde8
Diena1Dev <diena1dev@gmail.com>
2025-04-20 20:27:29 -0500

    Functional HUD Rendering, improved performance and reliability

--- src/client/java/com/diena1dev/mixins/TempMixin.java
+++ src/client/java/com/diena1dev/mixins/TempMixin.java
@@ -1,8 +1,5 @@
 package com.diena1dev.mixins;
 
-import com.diena1dev.crowutils.client.HUDLAYER;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,8 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TempMixin {
     @Inject(at = @At("HEAD"), method = "run")
     private void run(CallbackInfo info) {
-        System.out.println("CrowUtils Started, Registered 1 HUD Event!");
-        //HudLayerRegistrationCallback.EVENT.register( layeredDrawer -> layeredDrawer.attachLayerBefore(IdentifiedLayer.MISC_OVERLAYS, new HUDLAYER()));
+        System.out.println("CrowUtils Started!");
     }
 
 }

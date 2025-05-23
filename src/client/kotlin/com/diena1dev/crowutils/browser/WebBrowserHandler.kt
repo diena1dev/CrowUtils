32c57838102373c5f79ed163290a0412d359b5ad
diena1dev <diena1dev@gmail.com>
2025-05-23 00:49:31 -0500

    working extraction of coordinates through a *really* janky workaround!

--- src/client/kotlin/com/diena1dev/crowutils/browser/WebBrowserHandler.kt
+++ src/client/kotlin/com/diena1dev/crowutils/browser/WebBrowserHandler.kt
@@ -5,14 +5,6 @@ import com.cinemamod.mcef.MCEFBrowser
 import com.diena1dev.crowutils.client.gameInstance
 import com.diena1dev.crowutils.config.Config
 import org.apache.logging.log4j.LogManager
-import org.cef.CefSettings
-import org.cef.browser.CefBrowser
-import org.cef.callback.CefJSDialogCallback
-import org.cef.handler.CefDisplayHandler
-import org.cef.handler.CefDisplayHandlerAdapter
-import org.cef.handler.CefJSDialogHandler
-import org.cef.handler.CefJSDialogHandlerAdapter
-import org.cef.misc.BoolRef
 
 // TODO: On every JECF log message sent, check for logPointer, and if present, forward string to an internal var, for use in inserting into chat.
 
@@ -21,12 +13,13 @@ object WebBrowserHandler {
     lateinit var webBrowser: MCEFBrowser
     var c: Config = Config
     val logger = LogManager.getLogger()
+    var JSReaderHackResult: String = "---, ---"
+    var finalCoords: String? = "- -"
 
     fun init() {
         if (!this::webBrowser.isInitialized && MCEF.isInitialized()) {
             webBrowser = MCEF.createBrowser(Config.webHomePage, true)
-            webBrowser.client.addDisplayHandler(displayHandler)
-            logger.info("Browser Init Success!")
+            webBrowser.client.addJSDialogHandler(c.JSReaderHack)
         }
     }
 
@@ -34,7 +27,6 @@ object WebBrowserHandler {
         if (this::webBrowser.isInitialized) {
             c.hasInjectedCSS = false
             webBrowser.reload()
-            println("refreshBrowser called, value ${c.hasInjectedCSS}")
         }
     }
 
@@ -53,7 +45,7 @@ object WebBrowserHandler {
         }
     }
 
-    fun isBrowserInit(): Boolean {
+    fun isBrowserInit(): Boolean { // function to check if the browser was initialized
         var result: Boolean
         if (this::webBrowser.isInitialized) {
             result = true
@@ -63,136 +55,23 @@ object WebBrowserHandler {
         return result
     }
 
-    fun injectCSS() {
-        println("injectCSS called, value ${c.hasInjectedCSS}")
+    fun injectCSS() { // injection of a custom theme for the horizon's end dynmap, shrinking the worlds list substantially
         if (!c.hasInjectedCSS && isBrowserInit()) {
             c.hasInjectedCSS = true
-            webBrowser.executeJavaScript(
-                "const injectCSS = css => {" +
-                        "  let el = document.createElement('style');" +
-                        "  el.type = 'text/css';" +
-                        "  el.innerText = css;" +
-                        "  document.head.appendChild(el);" +
-                        "  return el;" +
-                        "};" +
-                        "injectCSS('/* By diena1dev (Crow) */" +
-                        "" +
-                        "/* Changes scale of and moves World icons */" +
-                        ".dynmap .sublist .item {" +
-                        "    display: block;" +
-                        "    float: right;" +
-                        "" +
-                        "scale:95%;" +
-                        "" +
-                        "    height: 18px;" +
-                        "    width: 18px;" +
-                        "" +
-                        "    padding: 2px;" +
-                        "    margin: -20px 3px 1px 1px;" +
-                        "" +
-                        "    border-radius: 1px;" +
-                        "    -moz-border-radius: 1px;" +
-                        "" +
-                        "    background: rgba(32,32,32,0.6);" +
-                        "    border: 1px solid rgba(64,64,64,0.6);" +
-                        "}" +
-                        "" +
-                        "/* Removes the bar across the list */" +
-                        ".dynmap .panel .subsection {" +
-                        "    display: block;" +
-                        "    clear: both;" +
-                        "" +
-                        "    width: 100%;" +
-                        "    line-height: 18px;" +
-                        "    margin: 0 0 7px 0;" +
-                        "" +
-                        "    border-bottom: 1px solid rgba(0,0,0,0);" +
-                        "" +
-                        "}" +
-                        "" +
-                        "/* This makes the selected World easy to see */" +
-                        ".dynmap .sublist .item.selected {" +
-                        "    background: rgba(128,128,128,0.5);" +
-                        "    border: 1px solid rgba(255,255,255,255);" +
-                        "}" +
-                        "" +
-                        "/* This disables the Compass */" +
-                        ".compass, .compass_NE, .compass_SE, .compass_NW, .compass_SW {" +
-                        "    display: block;" +
-                        "    position: absolute;" +
-                        "    z-index: 10;" +
-                        "    top: 0px;" +
-                        "    right: 0px;" +
-                        "    height: 0px;" +
-                        "    width: 0px;" +
-                        "    background-repeat: no-repeat;" +
-                        "}');", webBrowser.url, 1
+            webBrowser.executeJavaScript(c.CSSThemeScript, webBrowser.url, 1
             )
         }
     }
 
-    val displayHandler: CefDisplayHandler = object : CefDisplayHandlerAdapter() {
-        override fun onConsoleMessage(
-            browser: CefBrowser,
-            level: CefSettings.LogSeverity,
-            message: String,
-            source: String,
-            line: Int
-        ): Boolean {
-            lastConsoleMessage = "[$level] $message (at $source:$line)"
-            logger.info(lastConsoleMessage)
-            return false
-            super.onConsoleMessage(browser, level, message, source, line)
-        }
+    fun onJumpClick() { // executes dialog popup for JSReaderHack to pickup
+        webBrowser.executeJavaScript("window.alert(document.querySelector(\".coord-control-value\").textContent + \" logPointer\");", webBrowser.url, 100)
+        finalCoords = extractCoords(JSReaderHackResult)
+        logger.info(finalCoords)
     }
 
-    var lastConsoleMessage: String? = null
-
-    fun onJumpClick() {
-        webBrowser.executeJavaScript("(function() {" +
-                "  const levels = ['log', 'warn', 'error', 'info', 'debug'];" +
-                "  levels.forEach(function(level) {" +
-                "    const original = console[level];" +
-                "    console[level] = function(...args) {" +
-                "      original.apply(console, args);" +
-                "      try {" +
-                "        window.mcefQuery({" +
-                "          request: 'console:' + level + ':' + args.map(a => " +
-                "            (typeof a === 'object' ? JSON.stringify(a) : String(a))" +
-                "          ).join(' ')," +
-                "          persistent: false," +
-                "          onSuccess: function() {}," +
-                "          onFailure: function() {}" +
-                "        });" +
-                "      } catch (e) {}" +
-                "    };" +
-                "  });" +
-                "})();", webBrowser.url, 0)
-        webBrowser.executeJavaScript("console.log(document.querySelector(\".coord-control-value\").textContent + \" logPointer\");", webBrowser.url, 100)
-        webBrowser.executeJavaScript(
-            """
-    (function() {
-      const levels = ['log', 'warn', 'error', 'info', 'debug'];
-      levels.forEach(function(level) {
-        const original = console[level];
-        console[level] = function(...args) {
-          original.apply(console, args);
-          try {
-            window.mcefQuery({
-              request: 'console:' + level + ':' + args.map(a => 
-                (typeof a === 'object' ? JSON.stringify(a) : String(a))
-              ).join(' '),
-              persistent: false,
-              onSuccess: function() {},
-              onFailure: function() {}
-            });
-          } catch (e) {}
-        };
-      });
-    })();
-    """.trimIndent(),
-            webBrowser.url,
-            0
-        )
+    fun extractCoords(coords: String): String? {
+        val regex = Regex("""(\d+),\s*(\d+),\s*logPointer""")
+        val match = regex.find(coords)
+        return match?.let { "${it.groupValues[1]} ${it.groupValues[2]}" }
     }
 }
--- src/client/kotlin/com/diena1dev/crowutils/config/Config.kt
+++ src/client/kotlin/com/diena1dev/crowutils/config/Config.kt
@@ -1,5 +1,11 @@
 package com.diena1dev.crowutils.config
 
+import com.diena1dev.crowutils.browser.WebBrowserHandler
+import org.cef.browser.CefBrowser
+import org.cef.callback.CefJSDialogCallback
+import org.cef.handler.CefJSDialogHandler
+import org.cef.handler.CefJSDialogHandlerAdapter
+import org.cef.misc.BoolRef
 import org.lwjgl.glfw.GLFW
 
 @Suppress("unused")
@@ -40,6 +46,90 @@ object Config {
     val secondaryText = 0xFF137476
     val errorText = 0xFF165212
 
-    //Crow Purple
+    // Crow Purple
     val crowPurple = 0xFF147586
+
+    // Web Browser Snippets
+    // full credit to [oxmc](https://github.com/oxmc) for their help in finding a way to read JS outputs! :D
+    val JSReaderHack: CefJSDialogHandler = object : CefJSDialogHandlerAdapter() { // displayHandler workaround to get JS results from MCEF
+        override fun onJSDialog(
+            browser: CefBrowser?,
+            origin_url: String?,
+            dialog_type: CefJSDialogHandler.JSDialogType?,
+            message_text: String?,
+            default_prompt_text: String?,
+            callback: CefJSDialogCallback?,
+            suppress_message: BoolRef?
+        ): Boolean {
+            WebBrowserHandler.JSReaderHackResult = message_text.toString()
+            return super.onJSDialog(
+                browser,
+                origin_url,
+                dialog_type,
+                message_text,
+                default_prompt_text,
+                callback,
+                suppress_message
+            )
+        }
+    }
+    val CSSThemeScript = "\"const injectCSS = css => {\" +\n" +
+            "                        \"  let el = document.createElement('style');\" +\n" +
+            "                        \"  el.type = 'text/css';\" +\n" +
+            "                        \"  el.innerText = css;\" +\n" +
+            "                        \"  document.head.appendChild(el);\" +\n" +
+            "                        \"  return el;\" +\n" +
+            "                        \"};\" +\n" +
+            "                        \"injectCSS('/* By diena1dev (Crow) */\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"/* Changes scale of and moves World icons */\" +\n" +
+            "                        \".dynmap .sublist .item {\" +\n" +
+            "                        \"    display: block;\" +\n" +
+            "                        \"    float: right;\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"scale:95%;\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"    height: 18px;\" +\n" +
+            "                        \"    width: 18px;\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"    padding: 2px;\" +\n" +
+            "                        \"    margin: -20px 3px 1px 1px;\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"    border-radius: 1px;\" +\n" +
+            "                        \"    -moz-border-radius: 1px;\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"    background: rgba(32,32,32,0.6);\" +\n" +
+            "                        \"    border: 1px solid rgba(64,64,64,0.6);\" +\n" +
+            "                        \"}\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"/* Removes the bar across the list */\" +\n" +
+            "                        \".dynmap .panel .subsection {\" +\n" +
+            "                        \"    display: block;\" +\n" +
+            "                        \"    clear: both;\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"    width: 100%;\" +\n" +
+            "                        \"    line-height: 18px;\" +\n" +
+            "                        \"    margin: 0 0 7px 0;\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"    border-bottom: 1px solid rgba(0,0,0,0);\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"}\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"/* This makes the selected World easy to see */\" +\n" +
+            "                        \".dynmap .sublist .item.selected {\" +\n" +
+            "                        \"    background: rgba(128,128,128,0.5);\" +\n" +
+            "                        \"    border: 1px solid rgba(255,255,255,255);\" +\n" +
+            "                        \"}\" +\n" +
+            "                        \"\" +\n" +
+            "                        \"/* This disables the Compass */\" +\n" +
+            "                        \".compass, .compass_NE, .compass_SE, .compass_NW, .compass_SW {\" +\n" +
+            "                        \"    display: block;\" +\n" +
+            "                        \"    position: absolute;\" +\n" +
+            "                        \"    z-index: 10;\" +\n" +
+            "                        \"    top: 0px;\" +\n" +
+            "                        \"    right: 0px;\" +\n" +
+            "                        \"    height: 0px;\" +\n" +
+            "                        \"    width: 0px;\" +\n" +
+            "                        \"    background-repeat: no-repeat;\" +\n" +
+            "                        \"}');\""
 }
